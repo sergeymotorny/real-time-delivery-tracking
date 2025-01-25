5a926226e1848c72e55acf81863e074fb1363a0b
Sergey <motorny0919@gmail.com>
2025-01-25 21:51:34 +0200

    realization of the user

---
+++ UserApplication/src/main/java/com/motorny/service/LocationService.java
@@ -0,0 +1,14 @@
+package com.motorny.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+    @KafkaListener(topics = "delivery-location", groupId = "user-group")
+    public void deliveryLocation(String location) {
+        System.out.println(location);
+    }
+
+}
