70f10415441fafdb0c49480e757bc52fcc6592f9
Sergey <motorny0919@gmail.com>
2025-03-16 23:57:01 +0200

    adding logic for the Shipment model

--- UserApplication/src/main/java/com/motorny/models/ShipmentStatus.java
+++
@@ -1,6 +0,0 @@
-package com.motorny.models;
-
-public enum ShipmentStatus {
-
-    CREATED, PENDING, IN_TRANSIT, DELIVERED, CANCELED
-}
--- UserApplication/src/main/java/com/motorny/service/impl/ShipmentServiceImpl.java
+++ UserApplication/src/main/java/com/motorny/service/impl/ShipmentServiceImpl.java
@@ -1,46 +1,69 @@
 package com.motorny.service.impl;
 
+import com.motorny.dto.ShipmentDto;
+import com.motorny.exceptions.CourierNotFoundException;
+import com.motorny.exceptions.OrderNotFoundException;
 import com.motorny.exceptions.ShipmentNotFoundException;
+import com.motorny.mappers.ShipmentMapper;
+import com.motorny.models.Courier;
+import com.motorny.models.Order;
 import com.motorny.models.Shipment;
-import com.motorny.models.User;
-import com.motorny.models.enums.ShipmentStatus;
+import com.motorny.repositories.CourierRepository;
+import com.motorny.repositories.OrderRepository;
 import com.motorny.repositories.ShipmentRepository;
-import com.motorny.repositories.UserRepository;
 import com.motorny.service.ShipmentService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.List;
+
+import static com.motorny.models.enums.OrderStatus.CONFIRMED;
+import static com.motorny.models.enums.ShipmentStatus.IN_TRANSIT;
 
 @AllArgsConstructor
 @Service
 public class ShipmentServiceImpl implements ShipmentService {
 
     private final ShipmentRepository shipmentRepository;
-    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+    private final CourierRepository courierRepository;
+
+    private final ShipmentMapper shipmentMapper;
 
     @Override
-    public List<Shipment> getAllShipments() {
-        return shipmentRepository.findAll();
+    public List<ShipmentDto> getAllShipments() {
+        return shipmentRepository.findAll().stream()
+                .map(shipmentMapper::toShipmentDto)
+                .toList();
     }
 
     @Override
-    public void createShipment(Shipment shipment, Principal principal) {
-        String userEmail = principal.getName();
+    public ShipmentDto createShipmentForOrder(ShipmentDto shipmentDto, Long orderId, UserDetails userDetails) {
 
-        User user = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + userEmail));
+        String userByEmail = userDetails.getUsername();
+        Courier foundCourier = courierRepository.findByEmail(userByEmail)
+                .orElseThrow(() -> new CourierNotFoundException("User not found with email: " + userByEmail));
 
-        shipment.setStatus(ShipmentStatus.CREATED);
-        shipment.setCustomer(user);
-        shipmentRepository.save(shipment);
+        Order foundOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException("Order not found: " + orderId));
+
+        foundOrder.setStatus(CONFIRMED);
+
+        Shipment shipment = shipmentMapper.toShipment(shipmentDto);
+        shipment.setOrder(foundOrder);
+        shipment.setStatus(IN_TRANSIT);
+        shipment.setCourier(foundCourier);
+
+        Shipment savedShipment = shipmentRepository.save(shipment);
+
+        return shipmentMapper.toShipmentDto(savedShipment);
     }
 
     @Override
-    public Shipment getById(Long id) {
-        return shipmentRepository.findById(id)
-                .orElseThrow(() -> new ShipmentNotFoundException("Shipment not found exception"));
+    public ShipmentDto getById(Long id) {
+        Shipment shipment = shipmentRepository.findById(id)
+                .orElseThrow(() -> new ShipmentNotFoundException("Shipment not found: " + id));
+        return shipmentMapper.toShipmentDto(shipment);
     }
 }
