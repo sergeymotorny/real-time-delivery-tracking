dc32f007791228b23777c34f4319e9cf3e7e6030
Sergey <motorny0919@gmail.com>
2025-03-31 23:53:17 +0300

    fix for the problem of binding shipment to order

--- UserApplication/src/main/java/com/motorny/controllers/ShipmentController.java
+++ UserApplication/src/main/java/com/motorny/controllers/ShipmentController.java
@@ -1,17 +1,7 @@
 package com.motorny.controllers;
 
-import com.motorny.dto.ShipmentDto;
-import com.motorny.service.ShipmentService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
@@ -19,32 +9,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/shipments")
 public class ShipmentController {
 
-    private final ShipmentService shipmentService;
 
-    /*
-        Method for displaying page from Courier Orders lists
-     */
-
-    /*
-        The method when the courier will press "Take Order"
-     */
-
-
-    @GetMapping
-    public String show(Model model) {
-        model.addAttribute("shipments", new ShipmentDto());
-        return "/courier/courier_orders";
-    }
-
-    @PostMapping("/create")
-    public String createShipment(@Valid @ModelAttribute("shipment") ShipmentDto shipmentDto, BindingResult result,
-                                 Long orderId, @AuthenticationPrincipal UserDetails userDetails, Model model) {
-        if (result.hasErrors()) {
-            model.addAttribute("shipment", shipmentDto);
-            return "/courier/courier_orders";
-        }
-
-        shipmentService.createShipmentForOrder(shipmentDto, orderId, userDetails);
-        return "redirect:/courier/home";
-    }
 }
--- UserApplication/src/main/java/com/motorny/models/Courier.java
+++ UserApplication/src/main/java/com/motorny/models/Courier.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +20,16 @@ public class Courier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "courier")
+    private List<Shipment> shipments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "courier", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Location> locations = new ArrayList<>();
 }
 
 
