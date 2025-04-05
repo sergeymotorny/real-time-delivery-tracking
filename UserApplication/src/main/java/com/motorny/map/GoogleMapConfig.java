94261bfe62347cc778e29b1054f7f3cf96fc935d
Sergey <motorny0919@gmail.com>
2025-04-05 23:46:45 +0300

    starting work with GoogleMap

---
+++ UserApplication/src/main/java/com/motorny/map/GoogleMapConfig.java
@@ -0,0 +1,21 @@
+package com.motorny.map;
+
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleMapConfig {
+
+    @Value("${google.maps.api.key}")
+    private String apiKey;
+
+    @Bean
+    public GeoApiContext geoApiContext() {
+        return new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+}
