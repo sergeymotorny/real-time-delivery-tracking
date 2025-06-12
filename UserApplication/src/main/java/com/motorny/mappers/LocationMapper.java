8053b5df3c237b36a70297fec24c44b6a8caba44
Sergey <motorny0919@gmail.com>
2025-06-13 00:54:07 +0300

    create new Location classes

--- UserApplication/src/main/java/com/motorny/mappers/LocationMapper.java
+++ UserApplication/src/main/java/com/motorny/mappers/LocationMapper.java
@@ -3,8 +3,9 @@ package com.motorny.mappers;
 import com.motorny.dto.LocationDto;
 import com.motorny.models.Location;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface LocationMapper {
 
     LocationDto toLocationDto(Location location);
